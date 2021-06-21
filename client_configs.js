d1f46119b5d48244c5e85469a4bf189ae78feee2
Lemmin <lemminspam@comcast.net>
2021-06-21 00:12:51 -0700

    Initial commit.

---
+++ client_configs.js
@@ -0,0 +1,38 @@
+export default {
+	Alicia: {
+		"private_signing_key": "FRgm2FjEajyjZQyo_2NF6n-6FErQ4rDZbfQn7pa2YO35_fXEvl7sYQfpqS6EOe9oqZ29Ac_FvE2AkGlcxgEj0Q",
+		"public_signing_key": "-f31xL5e7GEH6akuhDnvaKmdvQHPxbxNgJBpXMYBI9E",
+		"private_key": "QAQVJBLkWVfMu7JGdKNDTzbJO4gp6MeXj8vyLRIW4d4",
+		"api_secret": "c9e53f387d71640350cd8e8d48a62ba9042dba646677c79a582997eadd9f5c1b",
+		"client_id": "4e446c9a-ced8-40fa-b962-1fff8d99747d",
+		"public_key": "GDwEIzNm5M_mNwqbV6-7PFApVziEfyzXt3Mij7D8XE4",
+		"version": "2",
+		"api_key_id": "93664dd2a8ccbd8d1d666008cb289a1afea57ec8a305f3916bc99ae7a5305785",
+		"api_url": "https://api.e3db.com",
+		"client_email": ""
+	},
+	Bruce: {
+		"private_signing_key": "jNT2XGqca7FP7OBXrUSmSqNnbdphfwvQnvUKA3IuWXAQacz6mNQgx8vl9LHPaZEYIzNxbvkAbVPcOMekEGYibw",
+		"public_signing_key": "EGnM-pjUIMfL5fSxz2mRGCMzcW75AG1T3DjHpBBmIm8",
+		"private_key": "FIBqgVyOMK2QMR4KmrhptIkLU2j0obFCBhtKvkF5LBA",
+		"api_secret": "c93f70252f167a90dfe78a92b9eea157f3c581f310519b58b6683af43c6447e5",
+		"client_id": "e1cfff9d-92e8-4406-b63f-829e43fbb47e",
+		"public_key": "Li6YAsVMxytY2MUZWk1s8-15puF_SohsJXzVaRsh3yk",
+		"version": "2",
+		"api_key_id": "4569a2faeb212778db6f47da67f68ef530ed6a6f75a586aa9b495cb1bb1e072e",
+		"api_url": "https://api.e3db.com",
+		"client_email": ""
+	},
+	Clarence: {
+		"private_signing_key": "4u9M2BhM1AU-cIrRoldKJVZ1buLOGSVfcyWq3zniECZfXjregMUg3t6U9Z-YZWIWe-FVXEDiTwZnuO0_LF2BaQ",
+		"public_signing_key": "X1463oDFIN7elPWfmGViFnvhVVxA4k8GZ7jtPyxdgWk",
+		"private_key": "QgqoTyWC9E-HKucueTofBgv3RXPsXu63VFhvDi_xFzs",
+		"api_secret": "efc8c3efaa0bf98dba088ff8f18fea2ed3154db672587c2b94da338c686d4c5c",
+		"client_id": "56a2b0f1-a93c-4503-88c2-dc5c2c5ea0f7",
+		"public_key": "d9LnB0VBlF8asKRJ68euKgTDltSO9H0zwqUxEQIiDRU",
+		"version": "2",
+		"api_key_id": "55fd0d237f477ef7364607b911f7832d493bec200ab1075eb140785bde034abf",
+		"api_url": "https://api.e3db.com",
+		"client_email": ""
+	}
+}
